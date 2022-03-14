720cc531581752fd9bc22435c6f279ed466802f8
vatsal220 <vatsal.p1997@gmail.com>
2022-03-13 22:46:08 -0400

    wrapped pipeline in if name = main format

--- levenshtein_distance/lev_dist.py
+++ levenshtein_distance/lev_dist.py
@@ -1,4 +1,9 @@
+import string
+import Levenshtein as lev
+import wikipedia
+
 from functools import lru_cache
+from nltk.corpus import stopwords
 
 def lev_dist(a, b):
     '''
@@ -37,33 +42,6 @@ def lev_dist(a, b):
 
     return min_dist(0, 0)
 
-a = 'stamp'
-b = 'stomp'
-lev_dist(a,b)
-
-import string
-import Levenshtein as lev
-import wikipedia
-
-from nltk.corpus import stopwords
-
-
-user_article = '''
-Identifying similarity between text is a common problem in NLP and is used by many companies world wide. The most common application of text similarity comes from the form of identifying plagiarized text. Educational facilities ranging from elementary school, high school, college and universities all around the world use services like Turnitin to ensure the work submitted by students is original and their own. Other applications of text similarity is commonly used by companies which have a similar structure to Stack Overflow or Stack Exchange. They want to be able to identify and flag duplicated questions so the user posting the question can be referenced to the original post with the solution. This increases the number of unique questions being asked on their platform. 
-Text similarity can be broken down into two components, semantic similarity and lexical similarity. Given a pair of text, the semantic similarity of the pair refers to how close the documents are in meaning. Whereas, lexical similarity is a measure of overlap in vocabulary. If both documents in the pairs have the same vocabularies, then they would have a lexical similarity of 1 and vice versa of 0 if there was no overlap in vocabularies [2].
-Achieving true semantic similarity is a very difficult and unsolved task in both NLP and Mathematics. It's a heavily researched area and a lot of the solutions proposed does involve a certain degree of lexical similarity in them. For the focuses of this article, I will not dive much deeper into semantic similarity, but focus a lot more on lexical similarity.
-Levenshtein Distance
-There are many ways to identify the lexical similarities between a pair of text, the one which we'll be covering today in this article is Levenshtein distance. An algorithm invented in 1965 by Vladimir Levenshtein, a Soviet mathematician [1].
-Intuition
-Levenshtein distance is very impactful because it does not require two strings to be of equal length for them to be compared. Intuitively speaking, Levenshtein distance is quite easy to understand.
-Informally, the Levenshtein distance between two words is the minimum number of single-character edits (insertions, deletions or substitutions) required to change one word into the other. [1]
-- https://en.wikipedia.org/wiki/Levenshtein_distance
-Essentially implying that the output distance between the two is the cumulative sum of the single-character edits. The larger the output distance is implies that more changes were necessary to make the two words equal each other, and the lower the output distance is implies that fewer changes were necessary. For example, given a pair of words dream and dream the resulting Levenshtein distance would be 0 because the two words are the same. However, if the words were dream and steam the Levenshtein distance would be 2 as you would need to make 2 edits to change dr to st .
-Thus a large value for Levenshtein distance implies that the two documents were not similar, and a small value for the distance implies that the two documents were similar.
-'''
-
-tags = ['Levenshtein distance']
-
 def fetch_wiki_data(tags):
     '''
     The purpose of this function is to get the wikipedia data associated to a certain user
@@ -93,8 +71,6 @@ def fetch_wiki_data(tags):
         content[tag] = page.content
     return content
 
-tag_content = fetch_wiki_data(tags)
-
 def remove_punctuations(txt, punct = string.punctuation):
     '''
     This function will remove punctuations from the input text
@@ -117,10 +93,6 @@ def clean_text(txt):
     txt = remove_punctuations(txt)
     txt = remove_stopwords(txt)
     return txt.lower()
-  
-user_article = clean_text(user_article) 
-for tag, content in tag_content.items():
-    tag_content[tag] = clean_text(content)
     
 def similarity(user_article, tag_content):
     '''
@@ -143,8 +115,6 @@ def similarity(user_article, tag_content):
         distances[tag] = dist
     return distances
   
-distances = similarity(user_article, tag_content)
-
 def is_plagiarism(user_article, tag_content, distances, th = 0.4):
     '''
     This function will identify if the user_article is considered plagiarized for each
@@ -170,4 +140,38 @@ def is_plagiarism(user_article, tag_content, distances, th = 0.4):
             distances[tag] = 'Not Plagiarized'
     return distances
   
-print(is_plagiarism(user_article, tag_content, distances))
+def main():
+    # Implemented Lev Distance
+    a = 'stamp'
+    b = 'stomp'
+    print(a, b)
+    print(lev_dist(a,b))
+
+    # Plagiarism Pipeline
+    user_article = '''
+    Identifying similarity between text is a common problem in NLP and is used by many companies world wide. The most common application of text similarity comes from the form of identifying plagiarized text. Educational facilities ranging from elementary school, high school, college and universities all around the world use services like Turnitin to ensure the work submitted by students is original and their own. Other applications of text similarity is commonly used by companies which have a similar structure to Stack Overflow or Stack Exchange. They want to be able to identify and flag duplicated questions so the user posting the question can be referenced to the original post with the solution. This increases the number of unique questions being asked on their platform. 
+    Text similarity can be broken down into two components, semantic similarity and lexical similarity. Given a pair of text, the semantic similarity of the pair refers to how close the documents are in meaning. Whereas, lexical similarity is a measure of overlap in vocabulary. If both documents in the pairs have the same vocabularies, then they would have a lexical similarity of 1 and vice versa of 0 if there was no overlap in vocabularies [2].
+    Achieving true semantic similarity is a very difficult and unsolved task in both NLP and Mathematics. It's a heavily researched area and a lot of the solutions proposed does involve a certain degree of lexical similarity in them. For the focuses of this article, I will not dive much deeper into semantic similarity, but focus a lot more on lexical similarity.
+    Levenshtein Distance
+    There are many ways to identify the lexical similarities between a pair of text, the one which we'll be covering today in this article is Levenshtein distance. An algorithm invented in 1965 by Vladimir Levenshtein, a Soviet mathematician [1].
+    Intuition
+    Levenshtein distance is very impactful because it does not require two strings to be of equal length for them to be compared. Intuitively speaking, Levenshtein distance is quite easy to understand.
+    Informally, the Levenshtein distance between two words is the minimum number of single-character edits (insertions, deletions or substitutions) required to change one word into the other. [1]
+    - https://en.wikipedia.org/wiki/Levenshtein_distance
+    Essentially implying that the output distance between the two is the cumulative sum of the single-character edits. The larger the output distance is implies that more changes were necessary to make the two words equal each other, and the lower the output distance is implies that fewer changes were necessary. For example, given a pair of words dream and dream the resulting Levenshtein distance would be 0 because the two words are the same. However, if the words were dream and steam the Levenshtein distance would be 2 as you would need to make 2 edits to change dr to st .
+    Thus a large value for Levenshtein distance implies that the two documents were not similar, and a small value for the distance implies that the two documents were similar.
+    '''
+
+    tags = ['Levenshtein distance']
+    tag_content = fetch_wiki_data(tags)
+
+    user_article = clean_text(user_article) 
+    for tag, content in tag_content.items():
+        tag_content[tag] = clean_text(content)
+        
+    distances = similarity(user_article, tag_content)
+    
+    print(is_plagiarism(user_article, tag_content, distances))
+
+if __name__ == '__main__':
+    main()
