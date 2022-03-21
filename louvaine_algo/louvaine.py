4d9708fdb3c8f3e31289460b0465bc924f6fd24f
vatsal220 <vatsal.p1997@gmail.com>
2022-03-21 01:32:41 -0400

    wrapped around if name = main function

--- louvaine_algo/louvaine.py
+++ louvaine_algo/louvaine.py
@@ -33,30 +33,32 @@ def generate_network(n):
     G = nx.MultiGraph(graph_dct)
     return G
 
-n = 500
-G = generate_network(n)
-print(nx.info(G))
+def main():
+    n = 500
+    G = generate_network(n)
+    print(nx.info(G))
 
-# visualize graph
-pos = nx.spring_layout(G)
-nx.draw(G, pos, node_size = 75, alpha = 0.8)
-plt.show()
+    # visualize graph
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, node_size = 75, alpha = 0.8)
+    plt.show()
 
+    comms = community_louvain.best_partition(G)
 
-comms = community_louvain.best_partition(G)
+    unique_coms = np.unique(list(comms.values()))
+    cmap = {
+        0 : 'maroon',
+        1 : 'teal',
+        2 : 'black', 
+        3 : 'orange',
+        4 : 'green',
+        5 : 'yellow'
+    }
 
-unique_coms = np.unique(list(comms.values()))
-cmap = {
-    0 : 'maroon',
-    1 : 'teal',
-    2 : 'black', 
-    3 : 'orange',
-    4 : 'green',
-    5 : 'yellow'
-}
+    node_cmap = [cmap[v] for _,v in comms.items()]
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, node_size = 75, alpha = 0.8, node_color=node_cmap)
+    plt.show()
 
-node_cmap = [cmap[v] for _,v in comms.items()]
-
-pos = nx.spring_layout(G)
-nx.draw(G, pos, node_size = 75, alpha = 0.8, node_color=node_cmap)
-plt.show()
+if __name__ == '__main__':
+    main()
